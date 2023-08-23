09d0ffad89c1271249bb840dd55c3618049e2b9a
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-08-23 11:45:30 +0700

    Update GamesPropsUtils.java

--- core/java/com/android/internal/util/evolution/GamesPropsUtils.java
+++ core/java/com/android/internal/util/evolution/GamesPropsUtils.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.internal.util.blaze;
+package com.android.internal.util.evolution;
 
 import android.app.Application;
 import android.content.Context;
